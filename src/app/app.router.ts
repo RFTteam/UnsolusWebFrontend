5452573eca25056f27174f97a15ddf08d14f3b4b
zoldkrisztian <krisztiangreen@gmail.com>
2017-10-24 15:24:45 +0200

    style

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -9,10 +9,14 @@ import { TeamsComponent } from './teams/teams.component';
 
 export const router: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full'},
-    { path: 'dota2', component: Dota2Component },
-    { path: 'league', component: LeagueComponent },
-    { path: 'players', component: PlayersComponent },
-    { path: 'teams', component: TeamsComponent }
+    { path: 'dota2', component: Dota2Component,children: [
+        {path: 'players', component: PlayersComponent },
+        { path: 'teams', component: TeamsComponent }
+    ] },
+    { path: 'league', component: LeagueComponent,children: [
+        {path: 'players', component: PlayersComponent },
+        { path: 'teams', component: TeamsComponent }
+    ] }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);

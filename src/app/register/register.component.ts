c9279e30d82e7865334c8e645d6729eafec7f47f
zoldkrisztian <krisztiangreen@gmail.com>
2017-10-30 14:55:45 +0100

    added shortcut

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService, UserService } from '../services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent {
+    model: any = {};
+    loading = false;
+
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
+
+    register() {
+        this.loading = true;
+        this.userService.create(this.model)
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
+
+
+/*import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -9,9 +44,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   userForm = new FormGroup({
-    name: new FormControl(' ',[Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+    name: new FormControl("",[Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
     email: new FormControl(),
-    password: new FormControl(),
+    password: new FormControl("",[Validators.required, Validators.minLength(5)]),
     birthday: new FormControl(null),
     country: new FormControl(),
     language: new FormControl()
@@ -26,3 +61,4 @@ export class RegisterComponent implements OnInit {
   }
 
 }
+*/
